28965882f579bf2ba510d2137ad18b741ae37e08
zhangdabao <18234101300@163.com>
2025-06-26 17:55:00 +0800

    删除code

--- src/pages/BookListUse.tsx
+++ src/pages/BookListUse.tsx
@@ -1,17 +1,10 @@
 import { List, Datagrid, TextField, DateField } from "react-admin";
-import { Button, Box } from "@mui/material";
+import { Box } from "@mui/material";
 import { usePermissions } from "react-admin";
-import { useNavigate } from "react-router";
 
 export const BookList = () => {
   const { permissions } = usePermissions();
   console.log("BookList Permissions:", permissions);
-  const navigate = useNavigate();
-
-  const handleCreate = () => {
-    navigate("/books/create");
-    console.log("Navigate to create page");
-  };
 
   const columns = [
     ...(permissions === "admin" ? [<TextField source="id" key="id" />] : []),
