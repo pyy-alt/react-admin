a0806fcddfd134dc5fbab8eaa14300ebd50fcdc5
zhangdabao <18234101300@163.com>
2025-07-04 16:43:06 +0800

    fix: vercel.json 2

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -2,15 +2,16 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import { fetchUtils } from "react-admin";
-import DataProvider from "ra-data-json-server";
+// import { fetchUtils } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-const httpClient = (url: string, options: RequestInit = {}) => {
-  if (!options.headers) {
-    options.headers = new Headers({ Accept: "application/json" });
-  }
-  return fetchUtils.fetchJson(url, options);
-};
+// const httpClient = (url: string, options: RequestInit = {}) => {
+//   if (!options.headers) {
+//     options.headers = new Headers({ Accept: "application/json" });
+//   }
+//   return fetchUtils.fetchJson(url, options);
+// };
 
 // export const dataProvider = DataProvider("http://localhost:3001", httpClient);
-export const dataProvider = DataProvider("/api", httpClient);
+
+export const dataProvider = jsonServerProvider("/api"); // 修改为 Vercel 的相对 API 路径
